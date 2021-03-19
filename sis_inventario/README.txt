cb26539ac5b506861d8c88c1463f2e7fa8cf1930
KryptoSpectrum <jomafu84@gmail.com>
2021-03-18 22:41:03 -0600

    Version 1

---
+++ sis_inventario/README.txt
@@ -0,0 +1,3 @@
+For view it, you need install Xampp
+pass:123
+Pass2:123456
---
+++ sis_inventario/vistas/modulos/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+
+	<strong>Copyright &copy; 2020 <a href="http://officesupplymanagua.com/" target="_blank">Librería Office Supply</a>.</strong>
+	<span>Software engineer: <a href="mailto:jomafu84@gmail.com" target="_blank">Jorge Martinez Fuentes</a>.</span>
+	<span><a href="Induccion.ppsx" target="_blank">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspAyuda?</a></span>
+
+
+
+</footer>
